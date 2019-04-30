ef046296ee2f2492fc1e3a5740c25d68fb02db28
Tyler Roberts <mcszymist7@gmail.com>
2019-04-30 09:55:47 -0800

    new

--- sobel-cpu.cu
+++ sobel-cpu.cu
@@ -1,8 +1,7 @@
-/*Samuel Grenon
+/*Tyler J Roberts
   CS441
   sobel- cpu.cu
-  A Sobel Filter using the gpu
-  with the template provided by Dr. Mock
+  Template Made by Dr. Mock
 /*
 
 
@@ -28,59 +27,53 @@
 #define threadsPerBlock 16
 // Returns the index into the 1d pixel array
 // Given te desired x,y, and image width
-__device__ int pixelIndex(int x, int y, int width)
-{
+__device__ int pixelIndex(const int &x,const int &y,const int &width){
     return (y*width + x);
 }
 
 // Returns the sobel value for pixel x,y
-__global__ void sobel(char *cpuPixels, int width, int height, char *pixels)
-{
-    int x = threadIdx.x + blockIdx.x * blockDim.x;
-    int y = threadIdx.y + blockIdx.y * blockDim.y;
+__global__ void sobel(char *cpuPixels,const int &width,const int &height, char *pixels){
+    const int x = threadIdx.x + blockIdx.x * blockDim.x;
+    const int y = threadIdx.y + blockIdx.y * blockDim.y;
     
-        int x00 = -1;  int x20 = 1;
-        int x01 = -2;  int x21 = 2;
-        int x02 = -1;  int x22 = 1;
-        x00 *= pixels[pixelIndex(x-1,y-1,width)];
-        x01 *= pixels[pixelIndex(x-1,y,width)];
-        x02 *= pixels[pixelIndex(x-1,y+1,width)];
-        x20 *= pixels[pixelIndex(x+1,y-1,width)];
-        x21 *= pixels[pixelIndex(x+1,y,width)];
-        x22 *= pixels[pixelIndex(x+1,y+1,width)];
-        
-        int y00 = -1;  int y10 = -2;  int y20 = -1;
-        int y02 = 1;  int y12 = 2;  int y22 = 1;
-        y00 *= pixels[pixelIndex(x-1,y-1,width)];
-        y10 *= pixels[pixelIndex(x,y-1,width)];
-        y20 *= pixels[pixelIndex(x+1,y-1,width)];
-        y02 *= pixels[pixelIndex(x-1,y+1,width)];
-        y12 *= pixels[pixelIndex(x,y+1,width)];
-        y22 *= pixels[pixelIndex(x+1,y+1,width)];
+	int x00 = -1;  int x20 = 1;
+	int x01 = -2;  int x21 = 2;
+	int x02 = -1;  int x22 = 1;
+	x00 *= pixels[pixelIndex(x-1,y-1,width)];
+	x01 *= pixels[pixelIndex(x-1,y,width)];
+	x02 *= pixels[pixelIndex(x-1,y+1,width)];
+	x20 *= pixels[pixelIndex(x+1,y-1,width)];
+	x21 *= pixels[pixelIndex(x+1,y,width)];
+	x22 *= pixels[pixelIndex(x+1,y+1,width)];
+	
+	int y00 = -1;  int y10 = -2;  int y20 = -1;
+	int y02 = 1;  int y12 = 2;  int y22 = 1;
+	y00 *= pixels[pixelIndex(x-1,y-1,width)];
+	y10 *= pixels[pixelIndex(x,y-1,width)];
+	y20 *= pixels[pixelIndex(x+1,y-1,width)];
+	y02 *= pixels[pixelIndex(x-1,y+1,width)];
+	y12 *= pixels[pixelIndex(x,y+1,width)];
+	y22 *= pixels[pixelIndex(x+1,y+1,width)];
 
-        int px = x00 + x01 + x02 + x20 + x21 + x22;
-        int py = y00 + y10 + y20 + y02 + y12 + y22;
-        cpuPixels[pixelIndex(x,y,width)] = sqrt(float(px*px + py*py));
+	const int px = x00 + x01 + x02 + x20 + x21 + x22;
+	const int py = y00 + y10 + y20 + y02 + y12 + y22;
+	cpuPixels[pixelIndex(x,y,width)] = sqrt(float(px*px + py*py));
 
 }
 
-int main()
-{
+int main(){
     FreeImage_Initialise();
     atexit(FreeImage_DeInitialise);
 
     // Load image and get the width and height
     FIBITMAP *image;
     image = FreeImage_Load(FIF_PNG, "coins.png", 0);
-    if (image == NULL)
-    {
+    if (image == NULL){
         printf("Image Load Problem\n");
         exit(0);
     }
-    int imgWidth;
-    int imgHeight;
-    imgWidth = FreeImage_GetWidth(image);
-    imgHeight = FreeImage_GetHeight(image);
+    const int imgWidth = FreeImage_GetWidth(image);
+    const int imgHeight = FreeImage_GetHeight(image);
 
     // Convert image into a flat array of chars with the value 0-255 of the
     // greyscale intensity
@@ -88,13 +81,13 @@ int main()
     char *pixels;  
     int pixIndex = 0;
     pixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
-    for (int i = 0; i < imgHeight; i++)
-     for (int j = 0; j < imgWidth; j++)
-     {
-       FreeImage_GetPixelColor(image,j,i,&aPixel);
-       char grey = ((aPixel.rgbRed + aPixel.rgbGreen + aPixel.rgbBlue)/3);
-       pixels[pixIndex++]=grey;
-     }
+    for (int i = 0; i < imgHeight; i++){
+		for (int j = 0; j < imgWidth; j++){
+			FreeImage_GetPixelColor(image,j,i,&aPixel);
+			char grey = ((aPixel.rgbRed + aPixel.rgbGreen + aPixel.rgbBlue)/3);
+			pixels[pixIndex++]=grey;
+		}
+	}
 
     dim3 numThreads(threadsPerBlock, threadsPerBlock, 1);
     dim3 numBlocks(ceil(imgWidth/threadsPerBlock), ceil(imgHeight/threadsPerBlock), 1);
@@ -103,26 +96,30 @@ int main()
     char *dev_pixels;
     char *dev_cpuPixel;
     char *resultPixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
+	
+	
     cudaMalloc((void**) &dev_pixels, sizeof(char)*imgWidth*imgHeight);
     cudaMalloc((void**) &dev_cpuPixel, sizeof(char)*imgWidth*imgHeight);
+	
     cudaMemcpy(dev_pixels, pixels, sizeof(char)*imgWidth*imgHeight, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_cpuPixel, resultPixels, sizeof(char)*imgWidth*imgHeight, cudaMemcpyHostToDevice);
+	
     sobel<<<numBlocks, numThreads>>>(dev_cpuPixel, imgWidth, imgHeight, dev_pixels);
+	
     cudaMemcpy(resultPixels, dev_cpuPixel, sizeof(char)*imgWidth*imgHeight, cudaMemcpyDeviceToHost);
-    for (int i = 1; i < imgWidth-1; i++)
-    {
-      for (int j = 1; j < imgHeight-1; j++)
-      {
-    // int sVal = sobel(i,j,imgWidth,pixels);
-        int sVal = float(resultPixels[j * imgWidth + i]);
-        aPixel.rgbRed = sVal;
-        aPixel.rgbGreen = sVal;
-        aPixel.rgbBlue = sVal;
-        FreeImage_SetPixelColor(bitmap, i, j, &aPixel);
-      }
+	
+    for (int i = 1; i < imgWidth-1; i++){
+		for (int j = 1; j < imgHeight-1; j++){
+			int sVal = float(resultPixels[j * imgWidth + i]);
+			aPixel.rgbRed = sVal;
+			aPixel.rgbGreen = sVal;
+			aPixel.rgbBlue = sVal;
+			FreeImage_SetPixelColor(bitmap, i, j, &aPixel);
+		}
     }
     FreeImage_Save(FIF_PNG, bitmap, "coins-edge.png", 0);
-  
+	cudaFree(dev_pixels);
+	cudaFree(dev_cpuPixel);
     free(pixels);
     FreeImage_Unload(bitmap);
     FreeImage_Unload(image);
